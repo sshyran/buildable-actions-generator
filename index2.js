6536b5e19ea5fbd83968c0913528cd4d6bbaeb1c
Mike Gindin <gindin.mike@gmail.com>
2022-02-11 13:49:45 -0500

    format index2.js and put required values in params/body first

--- index2.js
+++ index2.js
@@ -1,37 +1,40 @@
-const fs = require("fs")
-const { snakeCase } = require("snake-case")
-const { getBaseUrl, getFullPath, getHeadersArray, _getParameters, _getBodyParameters } = require("./parse-openapi")
-const openApi = JSON.parse(fs.readFileSync("../play/openapi-github.json"))
+const fs = require("fs");
+const { snakeCase } = require("snake-case");
+const {
+  getBaseUrl,
+  getFullPath,
+  getHeadersArray,
+  _getParameters,
+  _getBodyParameters,
+} = require("./parse-openapi");
+const openApi = JSON.parse(fs.readFileSync("../play/openapi-github.json"));
 
 let configFile = ({ name, title, description }) => ({
-  "name": name,
-  "title": title,
-  "description": description,
-  "type": "js-request-function",
-  "envVars": {
-    "GITHUB_API_TOKEN": {
-      "development": "",
-      "production": ""
+  name: name,
+  title: title,
+  description: description,
+  type: "js-request-function",
+  envVars: {
+    GITHUB_API_TOKEN: {
+      development: "",
+      production: "",
     },
-    "GITHUB_API_USERNAME": {
-      "development": "",
-      "production": ""
-    }
+    GITHUB_API_USERNAME: {
+      development: "",
+      production: "",
+    },
   },
-  "fee": 0,
-  "image": "https://assets.buildable.dev/catalog/node-templates/github.svg",
-  "category": "git",
-  "accessType": "open",
-  "language": "javascript",
-  "price": "free",
-  "platform": "github",
-  "tags": [
-    "git",
-    "github"
-  ],
-  "stateType": "stateless",
-  "__version": "1.0.0"
-})
+  fee: 0,
+  image: "https://assets.buildable.dev/catalog/node-templates/github.svg",
+  category: "git",
+  accessType: "open",
+  language: "javascript",
+  price: "free",
+  platform: "github",
+  tags: ["git", "github"],
+  stateType: "stateless",
+  __version: "1.0.0",
+});
 
 let inputFile = ({ title, docs, input }) => `
 /**
@@ -57,9 +60,18 @@ const nodeInput = ({ $trigger, $nodes }) => {
     ${input}
   };
 };
-`
+`;
 
-let runFile = ({ title, description, docs, input, axiosCall, verifyInput, verifyErrors, verifyChecks }) => `
+let runFile = ({
+  title,
+  description,
+  docs,
+  input,
+  axiosCall,
+  verifyInput,
+  verifyErrors,
+  verifyChecks,
+}) => `
 /**
  * ----------------------------------------------------------------------------------------------------
  * ${title} [Run]
@@ -111,26 +123,26 @@ const verifyInput = ({ ${verifyInput} }) => {
 
   ${verifyChecks}
 };
-`
+`;
 
 const run = async () => {
   for (let path in openApi.paths) {
     for (let method in openApi.paths[path]) {
+      const url =
+        getBaseUrl(openApi, path, method) + getFullPath(openApi, path, method);
 
-      const url = getBaseUrl(openApi, path, method) + getFullPath(openApi, path, method);
-
-      const headers = getHeadersArray(openApi, path, method)
+      const headers = getHeadersArray(openApi, path, method);
       //GitHub specific
       headers.push({
-        name: 'accept',
-        description: 'This API is under preview and subject to change.',
-        in: 'header',
+        name: "accept",
+        description: "This API is under preview and subject to change.",
+        in: "header",
         schema: {
-          type: 'string',
-          default: 'application/vnd.github.v3+json'
+          type: "string",
+          default: "application/vnd.github.v3+json",
         },
         required: true,
-      })
+      });
 
       //GitHub specific
       const auth = [
@@ -140,10 +152,10 @@ const run = async () => {
           field: "username",
           required: true, // not actually, but doesn't break if provide null values,
           schema: {
-            type: "string"
+            type: "string",
           },
           sample: "$trigger.env.GITHUB_API_USERNAME",
-          isEnvironmentVariable: true
+          isEnvironmentVariable: true,
         },
         {
           name: "GITHUB_API_TOKEN",
@@ -151,99 +163,196 @@ const run = async () => {
           in: "auth",
           required: true, // not actually, but doesn't break if provide null values,
           schema: {
-            type: "string"
+            type: "string",
           },
           sample: "$trigger.env.GITHUB_API_TOKEN",
-          isEnvironmentVariable: true
-        }
+          isEnvironmentVariable: true,
+        },
+      ];
 
-      ]
+      const params = _getParameters(openApi, path, method, {});
 
-      const params = _getParameters(openApi, path, method, {})
+      const body = _getBodyParameters(openApi, path, method);
 
-      const body = _getBodyParameters(openApi, path, method)
+      let title = openApi.paths[path][method].summary
+        .split(" ")
+        .map((i) => i.charAt(0).toUpperCase() + i.slice(1))
+        .join(" ");
 
+      let description =
+        openApi.paths[path][method].summary + " using the GitHub API";
 
-      
-      let title = openApi.paths[path][method].summary.split(" ").map(i => i.charAt(0).toUpperCase() + i.slice(1)).join(" ")
+      let docs = openApi.paths[path][method].externalDocs.url;
 
-      let description = openApi.paths[path][method].summary + " using the GitHub API"
+      let input = auth
+        .concat(params)
+        .concat(body)
+        .map((i) => i.name);
 
-      let docs = openApi.paths[path][method].externalDocs.url
+      let axiosData =
+        body.length > 0
+          ? `data: {${body
+              .sort((a, b) => { // sort required first
+                if (a.required) {
+                  return -1;
+                }
+                if (b.required) {
+                  return 1;
+                }
 
-      let input = auth.concat(params).concat(body).map(i => i.name)
-
-      let axiosData = body.length > 0 ? `data: {${body.map(i => i.required ? `${i.name}` : `...(${i.name} ? { ${i.name} } : {})`)}}` : ""
+                return 0;
+              })
+              .map((i) =>
+                i.required ? `${i.name}` : `...(${i.name} ? { ${i.name} } : {})`
+              )}}`
+          : "";
 
       let axiosCall = `
         method: "${method}",
         url: \`${url.replace(/{/g, "${")}\`,
-        auth: {${auth.map(i => `${i.field}: ${i.name}`).join(", ")}},
-        headers: {${headers.map(i => `${i.name}: "${i.schema.default}"`).join(", ")}},
-        params: {${params.filter(i => i.in === "query").map(i => i.required ? `${i.name}` : `...(${i.name} ? { ${i.name} } : {})`)}},
+        auth: {${auth.map((i) => `${i.field}: ${i.name}`).join(", ")}},
+        headers: {${headers
+          .map((i) => `${i.name}: "${i.schema.default}"`)
+          .join(", ")}},
+        params: {${params
+          .filter((i) => i.in === "query")
+          .sort((a, b) => { // sort required first
+            if (a.required) {
+              return -1;
+            }
+            if (b.required) {
+              return 1;
+            }
+
+            return 0;
+          })
+          .map((i) =>
+            i.required ? `${i.name}` : `...(${i.name} ? { ${i.name} } : {})`
+          )}},
         ${axiosData}
-      `
+      `;
 
-      let verifyInput = params.concat(body).filter(i => i.required).map(i => i.name)
-      let verifyErrors = params.concat(body).filter(i => i.required).map(i => `INVALID_${snakeCase(i.name).toUpperCase()}: "A valid ${i.name} field (${typeof i.sample}) was not provided in the input.",`).join("\n")
-      let verifyChecks = params.concat(body).filter(i => i.required).map(i => `if (typeof ${i.name} !== "${typeof i.sample}") throw new Error(ERRORS.INVALID_${snakeCase(i.name).toUpperCase()});`).join("\n")
+      let verifyInput = params
+        .concat(body)
+        .filter((i) => i.required)
+        .map((i) => i.name);
+      let verifyErrors = params
+        .concat(body)
+        .filter((i) => i.required)
+        .map(
+          (i) =>
+            `INVALID_${snakeCase(i.name).toUpperCase()}: "A valid ${
+              i.name
+            } field (${typeof i.sample}) was not provided in the input.",`
+        )
+        .join("\n");
+      let verifyChecks = params
+        .concat(body)
+        .filter((i) => i.required)
+        .map(
+          (i) =>
+            `if (typeof ${
+              i.name
+            } !== "${typeof i.sample}") throw new Error(ERRORS.INVALID_${snakeCase(
+              i.name
+            ).toUpperCase()});`
+        )
+        .join("\n");
 
-      let _runFile = runFile({ title, description, docs, input, axiosCall, verifyInput, verifyErrors, verifyChecks })
+      let _runFile = runFile({
+        title,
+        description,
+        docs,
+        input,
+        axiosCall,
+        verifyInput,
+        verifyErrors,
+        verifyChecks,
+      });
 
       function camelize(str) {
-        return str.replace(/(?:^\w|[A-Z]|\b\w)/g, function(word, index) {
-          return index === 0 ? word.toLowerCase() : word.toUpperCase();
-        }).replace(/\s+/g, '');
+        return str
+          .replace(/(?:^\w|[A-Z]|\b\w)/g, function (word, index) {
+            return index === 0 ? word.toLowerCase() : word.toUpperCase();
+          })
+          .replace(/\s+/g, "");
       }
 
-      let _configFile = configFile({ title, description, name: camelize(openApi.paths[path][method].summary.replace("-", " ")) + "Result" })
-
+      let _configFile = configFile({
+        title,
+        description,
+        name:
+          camelize(openApi.paths[path][method].summary.replace("-", " ")) +
+          "Result",
+      });
 
       const handleJSONSampleQuotes = (json) => {
-        return json.replace(/\uFFFF/g, '\\\"');
-      }
-      
+        return json.replace(/\uFFFF/g, '\\"');
+      };
 
-      let inputFileInput =
-      `
-      ${auth.concat(params).concat(body).filter(p => p.required).map(p => {
-        if(p.sample && typeof p.sample === "string") {
-          if(p.isEnvironmentVariable) {
-            return `${p.name}: ${p.sample && typeof p.sample === "object" ? handleJSONSampleQuotes(JSON.stringify(p.sample)) : p.sample}, // Required for private repos or if making structural changes (i.e modifying branch protection rules)`
+      let inputFileInput = `
+      ${auth
+        .concat(params)
+        .concat(body)
+        .filter((p) => p.required)
+        .map((p) => {
+          if (p.sample && typeof p.sample === "string") {
+            if (p.isEnvironmentVariable) {
+              return `${p.name}: ${
+                p.sample && typeof p.sample === "object"
+                  ? handleJSONSampleQuotes(JSON.stringify(p.sample))
+                  : p.sample
+              }, // Required for private repos or if making structural changes (i.e modifying branch protection rules)`;
+            }
+
+            return `${p.name}: "${p.sample.replace(/"/g, "")}", // Required`;
           }
 
-          return `${p.name}: "${p.sample.replace(/"/g, "")}", // Required`
-        }
-
-        return `${p.name}: ${p.sample && typeof p.sample === "object" ? handleJSONSampleQuotes(JSON.stringify(p.sample)) : p.sample}, // Required`
-      }).join("\n")}
+          return `${p.name}: ${
+            p.sample && typeof p.sample === "object"
+              ? handleJSONSampleQuotes(JSON.stringify(p.sample))
+              : p.sample
+          }, // Required`;
+        })
+        .join("\n")}
       
-      ${auth.concat(params).concat(body).filter(p => !p.required).map(p => {
-        if(p.sample && typeof p.sample === "string") {
-          if(p.isEnvironmentVariable) {
-            return `${p.name}: ${p.sample && typeof p.sample === "object" ? handleJSONSampleQuotes(JSON.stringify(p.sample)) : p.sample}, // Required for private repos or if making structural changes (i.e modifying branch protection rules)`
+      ${auth
+        .concat(params)
+        .concat(body)
+        .filter((p) => !p.required)
+        .map((p) => {
+          if (p.sample && typeof p.sample === "string") {
+            if (p.isEnvironmentVariable) {
+              return `${p.name}: ${
+                p.sample && typeof p.sample === "object"
+                  ? handleJSONSampleQuotes(JSON.stringify(p.sample))
+                  : p.sample
+              }, // Required for private repos or if making structural changes (i.e modifying branch protection rules)`;
+            }
+
+            return `// ${p.name}: "${p.sample.replace(/"/g, "")}",`;
           }
 
-          return `// ${p.name}: "${p.sample.replace(/"/g, "")}",`
-        }
+          return `// ${p.name}: ${
+            p.sample && typeof p.sample === "object"
+              ? handleJSONSampleQuotes(JSON.stringify(p.sample))
+              : p.sample
+          },`;
+        })
+        .join("\n")}
+      `;
 
-        return `// ${p.name}: ${p.sample && typeof p.sample === "object" ? handleJSONSampleQuotes(JSON.stringify(p.sample)) : p.sample},`
-      }).join("\n")}
-      `
+      let _inputFile = inputFile({ title, docs, input: inputFileInput });
 
-      let _inputFile = inputFile({ title, docs, input: inputFileInput })
+      let dir = `generated/${docs.split("#")[1]}`;
 
-      let dir = `generated/${docs.split("#")[1]}`
+      fs.mkdirSync(dir, { recursive: true });
 
-      fs.mkdirSync(dir, { recursive: true })
-
-      fs.writeFileSync(`${dir}/run.js`, _runFile)
-      fs.writeFileSync(`${dir}/config.json`, JSON.stringify(_configFile))
-      fs.writeFileSync(`${dir}/input.js`, _inputFile)
-      
+      fs.writeFileSync(`${dir}/run.js`, _runFile);
+      fs.writeFileSync(`${dir}/config.json`, JSON.stringify(_configFile));
+      fs.writeFileSync(`${dir}/input.js`, _inputFile);
     }
   }
+};
 
-}
-
-run()
+run();
