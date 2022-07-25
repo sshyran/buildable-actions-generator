13e554d96786b37c946a168538adf46275092d58
Paul Krishnamurthy <paul@paulkr.com>
2022-07-25 14:48:11 -0400

    Add spotify openapi spec + run config

--- index.js
+++ index.js
@@ -482,34 +482,116 @@ const run = async ({ baseURL, config, getParams, getTitle, getDescription, getDo
 
 
 run({
-  baseURL: `https://slack.com/api`,
+  baseURL: "{BUILDABLE_SPOTIFY_BASE_URI}", // can be hardcoded string (i.e https://my-api.com) and/or contain envVar replacement values (i.e https://{SOME_API_URL}/api)
   config: {
-    platform: "slack",
+    platform: "spotify",
     type: "js-request-function",
     envVars: {
-      BUILDABLE_SLACK_ACCESS_TOKEN: {
+      BUILDABLE_SPOTIFY_BASE_URI: {
+        development: "https://api.spotify.com/v1",
+        production: "https://api.spotify.com/v1",
+        in: "path"
+      },
+      BUILDABLE_SPOTIFY_CLIENT_ID: {
         development: "",
         production: "",
-        in: "header",
-        // name: "password",
-        headerName: "authorization"
+        in: "auth",
+        name: "username"
+      },
+      BUILDABLE_SPOTIFY_CLIENT_SECRET: {
+        development: "",
+        production: "",
+        in: "auth",
+        name: "password"
       }
     },
     fee: 0,
-    category: "communication",
+    category: "media",
     accessType: "open",
     language: "javascript",
     price: "free",
-    tags: ["business", "messaging", "chat"],
+    tags: ["music", "podcasts"],
     stateType: "stateless",
     __version: "1.0.0",
     connections: [
       {
-        id: "62d863cd0bd36f737a23f631",
+        id: "62d865290bd36f737a23f632",
         type: "integration"
       }
-    ]
+    ],
   },
-  pathOrURL: "./openapi-specs/slack2.json",
+  pathOrURL: "./openapi-specs/spotify.json",
   isURL: false,
+  getRunFile: ({
+    title,
+    description,
+    docs,
+    imports,
+    input,
+    url,
+    method,
+    axiosHeaders,
+    axiosAuth,
+    axiosParams,
+    axiosData,
+    verifyInput,
+    verifyErrors,
+    verifyChecks,
+  }) => {
+    return `
+    const axios = require("axios");
+    const qs = require("qs");
+    
+    const run = async (input) => {
+      const { ${input} } = input;
+    
+      verifyInput(input);
+    
+      try {
+        const { data: { access_token } } = await axios({
+          method: "post",
+          url: "https://accounts.spotify.com/api/token",
+          headers: { 
+            "Content-Type": "application/x-www-form-urlencoded" 
+          },
+          auth: {
+            username: BUILDABLE_SPOTIFY_CLIENT_ID,
+            password: BUILDABLE_SPOTIFY_CLIENT_SECRET
+          },
+          data: qs.stringify({ grant_type: "client_credentials" })
+        });
+        
+        const { ${input.includes("data") ? "data: _data" : "data"} } = await axios({
+          method: ${getTemplateString(method)},
+          url: ${getTemplateString(url)},
+          headers: {
+            Authorization: \`Bearer \${access_token}\`
+          },
+          ${[
+            axiosParams,
+            axiosData].filter(i => !!i.trim()).join(",\n")}
+        });
+    
+        return data;
+      } catch (error) {
+        return {
+          failed: true,
+          message: error.message,
+          data: error.response.data,
+        };
+      }
+    };
+    
+    /**
+     * Verifies the input parameters
+     */
+    const verifyInput = ({ ${verifyInput} }) => {
+      const ERRORS = {
+        ${verifyErrors}
+      };
+    
+      ${verifyChecks}
+    };`
+    
+  }
 })
--- run-input-samples.js
+++ run-input-samples.js
@@ -338,17 +338,27 @@ const notion = {
 }
 
 const spotify = {
-  baseURL: `https://{SPOTIFY_BASE_URI}`, // can be hardcoded string (i.e https://my-api.com) and/or contain envVar replacement values (i.e https://{SOME_API_URL}/api)
+  baseURL: "{SPOTIFY_BASE_URI}", // can be hardcoded string (i.e https://my-api.com) and/or contain envVar replacement values (i.e https://{SOME_API_URL}/api)
   config: {
     platform: "spotify",
     type: "js-request-function",
     envVars: {
-      BUILDABLE_SPOTIFY_ACCESS_TOKEN: {
+      SPOTIFY_BASE_URI: {
+        development: "https://api.spotify.com/v1",
+        production: "https://api.spotify.com/v1",
+        in: "path"
+      },
+      SPOTIFY_CLIENT_ID: {
         development: "",
         production: "",
-        in: "header",
-        // name: "password",
-        headerName: "authorization"
+        in: "auth",
+        name: "username"
+      },
+      SPOTIFY_CLIENT_SECRET: {
+        development: "",
+        production: "",
+        in: "auth",
+        name: "password"
       }
     },
     fee: 0,
@@ -359,15 +369,87 @@ const spotify = {
     tags: ["music", "podcasts"],
     stateType: "stateless",
     __version: "1.0.0",
-    connections: [
-      {
-        id: "62d8668e0bd36f737a23f633",
-        type: "integration"
-      }
-    ]
   },
-  pathOrURL: "./openapi-specs/spotify.json",
+  pathOrURL: "openapi.json",
   isURL: false,
+  connections: [
+    {
+      id: "62d865290bd36f737a23f632",
+      type: "integration"
+    }
+  ],
+  getRunFile: ({
+    title,
+    description,
+    docs,
+    imports,
+    input,
+    url,
+    method,
+    axiosHeaders,
+    axiosAuth,
+    axiosParams,
+    axiosData,
+    verifyInput,
+    verifyErrors,
+    verifyChecks,
+  }) => {
+    return `
+    const axios = require("axios");
+    const qs = require("qs");
+    
+    const run = async (input) => {
+      const { ${input} } = input;
+    
+      verifyInput(input);
+    
+      try {
+        const { data: { access_token } } = await axios({
+          method: "post",
+          url: "https://accounts.spotify.com/api/token",
+          headers: { 
+            "Content-Type": "application/x-www-form-urlencoded" 
+          },
+          auth: {
+            username: SPOTIFY_CLIENT_ID,
+            password: SPOTIFY_CLIENT_SECRET
+          },
+          data: qs.stringify({ grant_type: "client_credentials" })
+        });
+        
+        const { ${input.includes("data") ? "data: _data" : "data"} } = await axios({
+          method: ${getTemplateString(method)},
+          url: ${getTemplateString(url)},
+          headers: {
+            Authorization: \`Bearer \${access_token}\`
+          },
+          ${[
+            axiosParams,
+            axiosData].filter(i => !!i.trim()).join(",\n")}
+        });
+    
+        return data;
+      } catch (error) {
+        return {
+          failed: true,
+          message: error.message,
+          data: error.response.data,
+        };
+      }
+    };
+    
+    /**
+     * Verifies the input parameters
+     */
+    const verifyInput = ({ ${verifyInput} }) => {
+      const ERRORS = {
+        ${verifyErrors}
+      };
+    
+      ${verifyChecks}
+    };`
+    
+  }
 }
 
 const stripe = {
