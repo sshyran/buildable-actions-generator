9f18b5be7aa57af877ef965a9441ca8820cfa0db
Mike Gindin <gindin.mike@gmail.com>
2022-08-24 15:23:12 +0100

    fix stripe run input sample to remove html and using Stripe API ending for descriptions since it doesn't make sense sometimes

--- run-input-samples.js
+++ run-input-samples.js
@@ -483,6 +483,24 @@ const stripe = {
   },
   pathOrURL: "./openapi-specs/stripe-merged.json",
   isURL: false,
+  getTitle(openApi, path, method) {
+    let summary = openApi.paths[path][method].summary || openApi.paths[path][method].description || kebabCase(openApi.paths[path][method].operationId).replace(/-/g, " ") || `${method.toUpperCase()} ${path}`
+    summary = summary.replace(/<[^>]*>?/gm, ''); // clear any html tags
+    const periodIndex = summary.indexOf(".")
+    const endIndex = periodIndex === -1 ? 100 : periodIndex
+    const title = titleCase(summary).substring(0, endIndex)
+
+    return title
+  },
+  getDescription(openApi, path, method) {
+    let summary = openApi.paths[path][method].summary || openApi.paths[path][method].description || kebabCase(openApi.paths[path][method].operationId).replace(/-/g, " ") || `${method.toUpperCase()} ${path}`
+    summary = summary.replace(/<[^>]*>?/gm, ''); // clear any html tags
+    summary = summary.replace(/\n/g, "") // remove any new line character insertions in the text
+    summary = summary.replace(/\.(?=[A-Za-z])/g, ". ") // add a space after each period and non space character
+    const description = sentenceCase(summary).endsWith(".") ? sentenceCase(summary).slice(0, -1) : sentenceCase(summary)
+
+    return description
+  },
   getConfigName({ openApi, path, method }) {
     return camelize(openApi.paths[path][method].summary || openApi.paths[path][method].operationId) + "Result"
   },
